2e94c9ad5a7fbcb47f84d7e16e6327a88b6db7f7
Nuno Canto Brum <me@nunobrum.com>
2017-01-17 21:49:00 +0100

    Adding Histogram.py to the tool collection

--- Histogram.py
+++ Histogram.py
@@ -1,11 +1,10 @@
 # -------------------------------------------------------------------------------
-# Name:        LTSteps.py
-# Purpose:     Process LTSpice output files and align data for usage in a spread-
-#              sheet tool such as Excel, or Calc.
+# Name:        Histogram.py
+# Purpose:     Make an histogram plot based on the results of LTSpice.py
 #
 # Author:      Nuno Brum (nuno.brum@gmail.com)
 #
-# Created:     19-05-2014
+# Created:     17-01-2017
 # Licence:     Free
 # -------------------------------------------------------------------------------
 
@@ -23,7 +22,13 @@ opts = OptionParser(usage=usage, version="%prog 0.1")
 #opts.add_option('v', "var", action="store", type="string", dest="trace", help="The trace to be used in the histogram")
 opts.add_option('-s',"--sigma", action ="store", type="int", dest="sigma", default=3, help="Sigma to be used in the distribution fit. Default=3")
 opts.add_option('-n', "--nbins", action="store",  type="int", dest="nbins", default=20, help="Number of bins to be used in the histogram. Default=20")
-opts.add_option('-f', "--filter", action="append", type="string", dest="filters", help="Filter to a given setting step parameter")
+opts.add_option('-c', "--condition", action="append", type="string", dest="filters",
+                help="Filter condition writen in python. More than one expression can be added but each expression should be preceded by -f.\n" +
+                     "EXAMPLE: -c V(N001)>4 -c parameter==1 -c  I(V1)<0.5" )
+#opts.add_option('-f', "--format", action="store", type="string", dest="format", help="Format string for the X axis. Example: -f %3.4f")
+#opts.add_option('-p', "--scaling",action="store", type="string", dest="prescaling", help="Prescaling function to be applied to the input value.")
+opts.add_option('-t', "--title", action="store", type="string", dest="title", help="Title to appear on the top of the histogram.")
+opts.add_option('-r', "--range", action="store", type="string", dest="range", help="Range of the X axis to use for the histogram in the form min:max. Example: -r -1:1")
 
 (options, args) = opts.parse_args()
 
@@ -62,33 +67,24 @@ except ValueError:
     print("LOG FILE contains %s" % vars)
     exit(-1)
 
-filters = {}
-for filter in options.filters:
-    trace, value = filter.split("=", 2)
-    try:
-        filt_col = vars.index(trace)
-    except ValueError:
-        print ("Trace '%s' is not known." % trace)
-        continue
-
-    filters[filt_col] = float(value)
-
 
 values = []
-if len(filters) == 0:
+if (options.filters is None) or (len(options.filters) == 0):
     for line in log:
         #print(line)
         vs = line.split('\t')
         values.append(float(vs[sav_col]))
 else:
     for line in log:
-        vs = line.split('\t')
+        vs = map(float,line.split('\t'))
+        env = dict(zip(vars,vs))
 
-        for col_filt, value in  filters.items():
-            if float(vs[col_filt]) != value:
+        for expression in options.filters:
+            test = eval(expression, None, env)
+            if test == False:
                 break
         else:
-            values.append(float(vs[sav_col]))
+            values.append(env[TRACE])
 
 log.close()
 if len(values) == 0:
@@ -96,15 +92,32 @@ if len(values) == 0:
 elif len(values) < options.nbins:
     print("Not enough elements for an histogram")
 else:
-    x = np.array(values, dtype=float) * 100
+    x = np.array(values, dtype=float)
     mu = x.mean()
     mn = x.min()
     mx = x.max()
     sd = np.std(x)
     sigmin = mu - options.sigma*sd
     sigmax = mu + options.sigma*sd
-    axisXmin = mu - (options.sigma+1)*sd
-    axisXmax = mu + (options.sigma + 1) * sd
+
+    if options.range is None:
+        # Automatic calculation of the range
+        axisXmin = mu - (options.sigma+1)*sd
+        axisXmax = mu + (options.sigma + 1) * sd
+
+        if mn < axisXmin:
+            axisXmin = mn
+
+        if mx > axisXmax:
+            axisXmax = mx
+    else:
+        try:
+            smin, smax = options.range.split(":")
+            axisXmin = float(smin)
+            axisXmax = float(smax)
+        except:
+            opts.error("Invalid range setting")
+            exit(-1)
 
     print("Distributing in %d bins" % options.nbins)
     print("Minimum is %f" % mn)
@@ -118,10 +131,16 @@ else:
     # add a 'best fit' line
     y = mlab.normpdf( bins, mu, sd)
     l = plt.plot(bins, y, 'r--', linewidth=1)
-    plt.axvspan(mu - options.sigma*sd, mu + options.sigma*sd, alpha=0.2, color="red")
+    plt.axvspan(mu - options.sigma*sd, mu + options.sigma*sd, alpha=0.2, color="cyan")
     plt.xlabel(TRACE)
-    plt.ylabel('Probability')
-    plt.title(r'$\mathrm{Histogram\ of\ %s:}\ \mu=%f,\ \sigma=%d$' % (TRACE, mu, options.sigma))
+    plt.ylabel('Probability [normalized]')
+
+    if options.title is None:
+        title = r'$\mathrm{Histogram\ of\ %s:}\ \mu=%f,\ stdev=%f,\ \sigma=%d$' % (TRACE, mu, sd, options.sigma)
+    else:
+        title = options.title
+    plt.title(title)
+
     plt.axis([axisXmin, axisXmax, 0, axisYmax ])
     plt.grid(True)
     plt.show()
