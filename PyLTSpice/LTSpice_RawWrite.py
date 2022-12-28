c4a969d45a8369ac2427c1d98dac67917ece0f73
nunobrum <nuno.brum@gmail.com>
2022-12-29 00:17:45 +0100

    Legacy support for imports on LTSpice*.py files

---
+++ PyLTSpice/LTSpice_RawWrite.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+*(Deprecated)*
+Supporting Legacy import clauses. This will disappear in future versions
+"""
+
+print("Deprecation Warning! This will no longer be supported in future versions.\n"
+      "Use 'from PyLTSpice import RawWRead' for a direct import of the Raw Write class")
+
+from .RawWrite import *
--- PyLTSpice/__init__.py
+++ PyLTSpice/__init__.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# import RawRead
-# import RawWrite
-# import SpiceBatch
-
 # Convenience direct imports
 from .RawRead import RawRead, SpiceReadException
 from .RawWrite import RawWrite, Trace
-from .SpiceBatch import SpiceEditor, SimCommander
+from .SpiceEditor import SpiceEditor
+from .SpiceBatch import SimCommander
 
-# Compatibility with previous code
-LTSpice_RawRead = RawRead
-LTSpice_RawWrite = RawWrite
-LTSpiceBatch = SpiceBatch
