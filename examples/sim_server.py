ee55836c12d09be2ae4c8f63555dcadb3f0bbaab
Nuno Brum <nuno.brum@gmail.com>
2023-08-12 14:22:08 +0200

    Fixing the client server testbench

--- examples/sim_server.py
+++ examples/sim_server.py
@@ -1,10 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
 
-import time
-import PyLTSpice.sim.ltspice_simulator as ltsim
+# -------------------------------------------------------------------------------
+#    ____        _   _____ ____        _
+#   |  _ \ _   _| | |_   _/ ___| _ __ (_) ___ ___
+#   | |_) | | | | |   | | \___ \| '_ \| |/ __/ _ \
+#   |  __/| |_| | |___| |  ___) | |_) | | (_|  __/
+#   |_|    \__, |_____|_| |____/| .__/|_|\___\___|
+#          |___/                |_|
+#
+# Name:        run_server.py
+# Purpose:     A Command Line Interface to run the LTSpice Server
+#
+# Author:      Nuno Brum (nuno.brum@gmail.com)
+#
+# Created:     10-08-2023
+# Licence:     refer to the LICENSE file
+# -------------------------------------------------------------------------------
+import sys
+
 from PyLTSpice.client_server.sim_server import SimServer
+import time
+import keyboard
 
-a = SimServer(ltsim.LTspice, port=9000)
-while a.running():
+from PyLTSpice.sim.ltspice_simulator import LTspice
+simulator = LTspice
+
+print("Starting Server")
+server = SimServer(simulator, parallel_sims=4, output_folder='./temp', port=9000)
+print("Server Started. Press and hold 'q' to stop")
+while server.running():
     time.sleep(0.2)
-
-print("stopping...SimServer")
+    # Check whether a key was pressed
+    if keyboard.is_pressed('q'):
+        server.stop_server()
+        break
